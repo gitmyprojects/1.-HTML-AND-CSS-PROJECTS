6bcb6ee3791855fbb09fe763b3c9cbf388d00769
Neil Javier <82540095+gitmyprojects@users.noreply.github.com>
2022-03-14 20:11:02 -0700

    initial commit

---
+++ HTML_and_JavaScript/hello_world.js
@@ -0,0 +1,7 @@
+alert("Greetings!");
+
+function function_one() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElementById("irish").innerHTML = result;
+}
